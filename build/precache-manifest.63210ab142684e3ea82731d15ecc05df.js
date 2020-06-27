9f237de1564d417a5e143209a6add7670191b7ae
Melissa White <melissawhite176@gmail.com>
2020-06-27 10:27:13 -0700

    create production build, serve static files - phonebook backend (step 9 and 10)

---
+++ build/precache-manifest.63210ab142684e3ea82731d15ecc05df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d398ef8fefe566f1637acf94eabd56d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ba94569f13f6c7eb17c",
+    "url": "/static/css/main.627a0211.chunk.css"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba94569f13f6c7eb17c",
+    "url": "/static/js/main.4230c1fb.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
