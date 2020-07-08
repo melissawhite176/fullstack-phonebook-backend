6cb3b7b6488795e94bf498b3fecf17e41ae2b655
Melissa White <melissawhite176@gmail.com>
2020-07-07 19:05:19 -0700

    add run validator option for put request - phonebook (part 3d)

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number,
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, 
+        { new: true, runValidators: false})
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
